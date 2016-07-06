03246baaed9503a1b56d5b4e81a9c57fce440eb8
Nick Dyer <nbdyer@gmail.com>
2016-07-06 15:34:57 +0100

    Passing test for chat client connecting to serversocket

--- src/main/java/uk/nickbdyer/chatclient/ChatClient.java
+++ src/main/java/uk/nickbdyer/chatclient/ChatClient.java
@@ -1,40 +1,16 @@
 package uk.nickbdyer.chatclient;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class ChatClient {
 
+    public Socket connect(String hostname, int port) {
+        try {
+            return new Socket("localhost", 4444);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
 }
 
-//public class EchoClient {
-//    public static void main(String[] args) throws IOException {
-//
-//        if (args.length != 2) {
-//            System.err.println(
-//                    "Usage: java EchoClient <host name> <port number>");
-//            System.exit(1);
-//        }
-//
-//        String hostName = args[0];
-//        int portNumber = Integer.parseInt(args[1]);
-//
-//        try (
-//                Socket echoSocket = new Socket(hostName, portNumber);
-//                PrintWriter out =
-//                        new PrintWriter(echoSocket.getOutputStream(), true);
-//                BufferedReader stdIn =
-//                        new BufferedReader(
-//                                new InputStreamReader(System.in))
-//        ) {
-//            String userInput;
-//            while ((userInput = stdIn.readLine()) != null) {
-//                out.println(userInput);
-//            }
-//        } catch (UnknownHostException e) {
-//            System.err.println("Don't know about host " + hostName);
-//            System.exit(1);
-//        } catch (IOException e) {
-//            System.err.println("Couldn't get I/O for the connection to " +
-//                    hostName);
-//            System.exit(1);
-//        }
-//    }
-//}
--- src/test/java/uk/nickbdyer/chatclient/ChatClientTest.java
+++ src/test/java/uk/nickbdyer/chatclient/ChatClientTest.java
@@ -1,4 +1,21 @@
 package uk.nickbdyer.chatclient;
 
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static org.junit.Assert.assertTrue;
+
 public class ChatClientTest {
+
+    @Test
+    public void clientCanConnectToServerSocket() throws IOException {
+        new ServerSocket(4444);
+
+        Socket clientSocket = new ChatClient().connect("localhost", 4444);
+
+        assertTrue(clientSocket.isConnected());
+    }
 }
